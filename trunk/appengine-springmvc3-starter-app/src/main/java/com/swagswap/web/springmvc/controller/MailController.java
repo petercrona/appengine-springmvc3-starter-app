c5eb4c4280605d8c698caec2c9afa950683c557c
Sambrodkin <Sambrodkin@87de0bee-e3ef-11de-a6a9-d5aeed48c2a6>
2009-12-10 21:45:50 +0000

    formatting

--- trunk/appengine-springmvc3-starter-app/src/main/java/com/swagswap/web/springmvc/controller/MailController.java
+++ trunk/appengine-springmvc3-starter-app/src/main/java/com/swagswap/web/springmvc/controller/MailController.java
@@ -30,60 +30,63 @@ import com.swagswap.exceptions.ImageTooLargeException;
 import com.swagswap.exceptions.LoadImageFromURLException;
 import com.swagswap.service.SwagItemService;
 
-
 @Controller
 public class MailController {
 	private static final Logger log = Logger.getLogger(MailController.class);
-	//This email has to be registered as an admin in the appengine project you deploy to
-	//or it won't allow sending from this address
+	// This email has to be registered as an admin in the appengine project you
+	// deploy to or it won't allow sending from this address
 	private static final String ADMIN_EMAIL = "gaespringstarterapp@gmail.com";
 
 	@Autowired
 	private SwagItemService itemService;
-	
+
 	/**
 	 * Handle incoming email see
-	 * http://code.google.com/appengine/docs/java/mail/receiving.html	
+	 * http://code.google.com/appengine/docs/java/mail/receiving.html
 	 * 
 	 * Create an item from the email and notify the sender of item creation
 	 * 
 	 */
 	@RequestMapping(value = "/_ah/mail/{address}", method = RequestMethod.POST)
-	public void createSwagItemFromIncomingEmail(@PathVariable("address") String address,
-			HttpServletRequest request, HttpServletResponse response) {
+	public void createSwagItemFromIncomingEmail(@PathVariable("address")
+	String address, HttpServletRequest request, HttpServletResponse response) {
 		String fromEmail = null;
 		try {
 			// Create MimeMessage from request
-			// From Appengine docs: http://code.google.com/appengine/docs/java/mail/receiving.html
+			// From Appengine docs:
+			// http://code.google.com/appengine/docs/java/mail/receiving.html
 			Properties props = new Properties();
 			Session session = Session.getDefaultInstance(props, null);
-			MimeMessage mimeMessage = new MimeMessage(session, request.getInputStream());
-			
+			MimeMessage mimeMessage = new MimeMessage(session, request
+					.getInputStream());
+
 			String mailSubject = mimeMessage.getSubject();
-			
-			fromEmail = ((InternetAddress)mimeMessage.getFrom()[0]).getAddress();
-			log.debug("Receieved message from " + fromEmail +  " subject " + mailSubject);
-	
-			//get gody and attachment
-			//from http://jeremyblythe.blogspot.com/2009/12/gae-128-fixes-mail-but-not-jaxb.html
+
+			fromEmail = ((InternetAddress) mimeMessage.getFrom()[0])
+					.getAddress();
+			log.debug("Receieved message from " + fromEmail + " subject "
+					+ mailSubject);
+
+			// get gody and attachment 
+			// from http://jeremyblythe.blogspot.com/2009/12/gae-128-fixes-mail-but-not-jaxb.html
 			Object content = mimeMessage.getContent();
-			
-	        String bodyText = "";
-	        byte[] imageData = null;
-	        if (content instanceof String) {
-	            bodyText = (String)content;
-	        } else if (content instanceof Multipart) {
-	            Multipart multipart = (Multipart)content;
-	            Part part = multipart.getBodyPart(0);
-	            Object partContent = part.getContent();
-	            if (partContent instanceof String) {
-	                bodyText = (String)partContent;
-	            }
-	            //extract attached image if any
-	            imageData = getMailAttachmentBytes(multipart);
-	        }
-			
-	        //create item from mail
+
+			String bodyText = "";
+			byte[] imageData = null;
+			if (content instanceof String) {
+				bodyText = (String) content;
+			} else if (content instanceof Multipart) {
+				Multipart multipart = (Multipart) content;
+				Part part = multipart.getBodyPart(0);
+				Object partContent = part.getContent();
+				if (partContent instanceof String) {
+					bodyText = (String) partContent;
+				}
+				// extract attached image if any
+				imageData = getMailAttachmentBytes(multipart);
+			}
+
+			// create item from mail
 			SwagItem swagItem = new SwagItem();
 			swagItem.setOwner(fromEmail);
 			swagItem.setName(mailSubject);
@@ -91,90 +94,100 @@ public class MailController {
 			swagItem.setImageBytes(imageData);
 			itemService.save(swagItem);
 			sendItemAddedSuccessfullyEmail(fromEmail, swagItem);
-		}
-		catch (Exception e) {
-			log.error("Problem with incoming message from " + fromEmail,e);
-			//report error to sender
+		} catch (Exception e) {
+			log.error("Problem with incoming message from " + fromEmail, e);
+			// report error to sender
 			sendItemAddExceptionEmail(fromEmail, e);
-			//report error to Sam
+			// report error to Sam
 			sendItemAddExceptionEmailToAdmin(e);
-		}
-		finally {
-			//this is for if this method is called from TaskQueues (it's not right now)
-			//always send status OK or Appengine Task Queues will keep retrying
+		} finally {
+			// this is for if this method is called from TaskQueues (it's not
+			// right now)
+			// always send status OK or Appengine Task Queues will keep retrying
 			response.setStatus(HttpServletResponse.SC_OK);
 		}
 	}
 
 	/**
-	 * From http://java.sun.com/developer/onlineTraining/JavaMail/contents.html#JavaMailMessage
+	 * From
+	 * http://java.sun.com/developer/onlineTraining/JavaMail/contents.html#JavaMailMessage
+	 * 
 	 * @param attachmentInputStream
 	 * @param mimeMultipart
 	 * @return image data from attachment or null if there is none
 	 * @throws MessagingException
 	 * @throws IOException
 	 */
-	private byte[] getMailAttachmentBytes(Multipart mimeMultipart) throws MessagingException, IOException {
+	private byte[] getMailAttachmentBytes(Multipart mimeMultipart)
+			throws MessagingException, IOException {
 		InputStream attachmentInputStream = null;
 		try {
 			for (int i = 0, n = mimeMultipart.getCount(); i < n; i++) {
-				String disposition = mimeMultipart.getBodyPart(i).getDisposition();
+				String disposition = mimeMultipart.getBodyPart(i)
+						.getDisposition();
 				if (disposition == null) {
 					continue;
 				}
-				if ((disposition.equals(Part.ATTACHMENT) || (disposition.equals(Part.INLINE)))) {
-					attachmentInputStream = mimeMultipart.getBodyPart(i).getInputStream();
+				if ((disposition.equals(Part.ATTACHMENT) || (disposition
+						.equals(Part.INLINE)))) {
+					attachmentInputStream = mimeMultipart.getBodyPart(i)
+							.getInputStream();
 					byte[] imageData = getImageDataFromInputStream(attachmentInputStream);
 					return imageData;
 				}
 			}
-		}
-		finally {
-			try {if (attachmentInputStream!=null) attachmentInputStream.close();} catch (Exception e) {}
+		} finally {
+			try {
+				if (attachmentInputStream != null)
+					attachmentInputStream.close();
+			} catch (Exception e) {
+			}
 		}
 		return null;
 	}
 
-
 	private void sendItemAddExceptionEmail(String fromEmail, Exception e) {
-		send(fromEmail,"Your Item email upload failed :(",
-			"<b>Please <a href=\"http://code.google.com/p/springstarterapp/issues/entry?template=Defect%20report%20from%20user\">" +
-				"report this issue</a> (requires a google account)</b><br/><br/>Exception:<br/>" + e.toString()
-		);
-	}
-	
-	private void sendItemAddExceptionEmailToAdmin(Exception e) {
-		send(ADMIN_EMAIL,"SwagItem email upload failed :(",
-				e.toString()
-			);
+		send(
+				fromEmail,
+				"Your Item email upload failed :(",
+				"<b>Please <a href=\"http://code.google.com/p/springstarterapp/issues/entry?template=Defect%20report%20from%20user\">"
+						+ "report this issue</a> (requires a google account)</b><br/><br/>Exception:<br/>"
+						+ e.toString());
 	}
 
-	private void sendItemAddedSuccessfullyEmail(String fromEmail, SwagItem swagItem) {
-		send(fromEmail,
-			"Your item: " + swagItem.getName()
-					+ " has been successfuly created.  You may want to fill in additional item information",
-			"\n\n<br/><br/>See Your Item here: http://springstarterapp.appspot.com/springmvc/edit/"
-					+ swagItem.getKey()
-			);
+	private void sendItemAddExceptionEmailToAdmin(Exception e) {
+		send(ADMIN_EMAIL, "SwagItem email upload failed :(", e.toString());
 	}
-	
+
+	private void sendItemAddedSuccessfullyEmail(String fromEmail,
+			SwagItem swagItem) {
+		send(
+				fromEmail,
+				"Your item: "
+						+ swagItem.getName()
+						+ " has been successfuly created.  You may want to fill in additional item information",
+				"\n\n<br/><br/>See Your Item here: http://springstarterapp.appspot.com/springmvc/edit/"
+						+ swagItem.getKey());
+	}
+
 	public void send(String email, String subject, String msgBody) {
 		log.debug("Processing send for " + subject + " at " + new Date());
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-        try {
-            Message msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(ADMIN_EMAIL));
-            msg.addRecipient(Message.RecipientType.TO,new InternetAddress(email));
-            msg.setSubject(subject);
-	        msg.setContent(msgBody,"text/html");
-            Transport.send(msg);
-            log.debug("sending mail to " + email);
-        } catch (Exception e) {
-           log.error("sender is " +  ADMIN_EMAIL, e);
-        }
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(ADMIN_EMAIL));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					email));
+			msg.setSubject(subject);
+			msg.setContent(msgBody, "text/html");
+			Transport.send(msg);
+			log.debug("sending mail to " + email);
+		} catch (Exception e) {
+			log.error("sender is " + ADMIN_EMAIL, e);
+		}
 	}
-	
+
 	public byte[] getImageDataFromInputStream(InputStream inputStream)
 			throws LoadImageFromURLException, ImageTooLargeException {
 		BufferedInputStream bis = null;
